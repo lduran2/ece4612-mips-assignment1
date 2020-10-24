8a25c5aa4c675913aa2eb2da859d29e30be138d8
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2020-10-24 09:08:51 -0400

    Task 3, v1.5 Implemented the error from digraph using flags.

--- 3-check-matlab.asm
+++ 3-check-matlab.asm
@@ -13,6 +13,12 @@
 #
 # ChangeLog
 ######################################################################
+#	(v1.5) - 2020-10-24 t13:07Z
+#		Implemented the error from digraph using flags.
+#
+# 	(v1.4) - 2020-09-30 t07:44Z
+# 		Implemented the error from digraph ends using linking.
+#
 # 	v1.3 - 2020-09-30 t03:34Z
 # 		Implemented the "no operators since" flags.
 #
@@ -21,6 +27,8 @@
 #
 # 	v1.0 - 2020-09-29 t23:21Z
 # 		Implemented character validator.
+#
+#	Versions in parentheses are defunct.
 ######################################################################
 
 # command constants
@@ -34,6 +42,7 @@
 .eqv	flParError	64	# flags uneven parentheses
 .eqv	flOpndOptr	1	# flags no operator since operand
 .eqv	flClPrOptr	2	# flags no operator since closed parenthesis
+.eqv	flDgSt    	1	# flags digraph start
 
 .text	# the code block
 
@@ -63,21 +72,13 @@ inpChkInvalid:
 	la	$a0, invMessage          	# load address of invalid message
 	addi	$v0, $zero, print        	# print command
 	syscall	# print the message
+	j inpChkOutput	# don't print the character index
+inpChkIndex:	# used for debugging
 	la	$a1, invMessage          	# load  length of invalid message
 	add	$s0, $v1, $zero          	#  copy the error information
 	andi	$a0, $s0, lastchar       	#  copy line number to print
 	addi	$v0, $zero, intprint     	# print line number
 	syscall	# print the line number
-	andi	$a0, $s0, flParError     	# copy the parentheses error
-	bne	$a0, $zero, inpChkInvPar  	# branch if parentheses error
-	j	inpChkOutput             	# finish the loop
-inpChkInvPar:
-	addi	$a0, $zero, '\n'         	#  load newline
-	addi	$v0, $zero, chrprint     	# print newline
-	syscall	# print newline
-	la	$a0, parErrMsg           	# load address of parentheses error message
-	addi	$v0, $zero, print        	# print command
-	syscall	# print the message
 	j	inpChkOutput             	# finish the loop
 inpChkOutput:
 	addi	$a0, $zero, '\n'         	#  load newline
@@ -126,13 +127,27 @@ matchk: # matchk(char *X) : void
 	# $t5 := counter, i_parentheses
 	#                 of open parentheses - close parentheses
 	# $t6 := flags for operator between
-	add	$v1, $zero, $zero     	# clear $v1
-	add	$t0, $zero, $zero       # for k = 0,
-	add	$t5, $zero, $zero       # i_parentheses = 0
-	add	$t6, $zero, $zero      	# clear operator between flags
+	# $t7 := flags for digraph start and end
+	#
+	# flow:
+	# 	[(] -> dgst -> val
+	# 	[)] -> dgfn -> val
+	# 	[/*] -> dgfn -> dgst -> optr -> val
+	# 	[+-] -> dgst -> optr -> val
+	# 	[=] -> optr -> val
+	# 	[0-9A-Za-z] -> opnd -> val
+	#
+	# (dgst, dgfn) are flags, checked in val, rather than
+	# intermediate routines
+	#
+	and	$v1, $zero, $zero     	# clear $v1
+	or	$t0, $zero, $zero       # for k = 0,
+	or	$t5, $zero, $zero       # i_parentheses = 0
+	and	$t6, $zero, $zero      	# clear operator between flags
+	and	$t7, $zero, $zero      	# clear the digraph flags
 matChkL1:
-	add	$t1, $t0, $a1           # find X + k
-	lb	$t2, 0($t1)             # get X[k] alias *(X + k)
+	add	$t1, $t0, $a1          	# find X + k
+	lb	$t2, 0($t1)            	# get X[k] alias *(X + k)
 	beq	$t2, $zero, matChkValid	# if (end of string), then valid
 	addi	$t4, $zero, '\n'       	# load newline
 	beq	$t2, $t4, matChkValid	# if (X[k] == newline), then valid
@@ -140,15 +155,12 @@ matChkL1:
 matChkChVal:	# character is valid
 	addi	$t0, $t0, 1            	# ++k
 	j	matChkL1               	# next k
-matChkUePar:	# Uneven parentheses
-	ori	$v1, $v1, flParError   	# set the flParError flag
-	j	matChkInval            	# uneven parentheses are invalid
 matChkInval:	# string is not valid
-	add	$v0, $zero, $zero      	# clear valid flag
+	and	$v0, $zero, $zero      	# clear valid flag
 	or	$v1, $v1, $t0          	# $v1 |= k
 	j	rMatChk                	# finish
 matChkValid:
-	bne	$t5, $zero, matChkUePar	# invalid if parentheses uneven
+	bne	$t5, $zero, matChkInval	# invalid if parentheses uneven
 	addi	$v0, $zero, 1          	#   set valid flag
 rMatChk:
 	jr	$ra                    	# return to caller
@@ -160,27 +172,27 @@ rMatChk:
 #    , after +, : after 9, [ after Z, { after z
 matChkCharRng:
 matChkCharR10:	# character range 10 [(-+]
-	addi	$t4, $zero, '('        	# load '('
+	ori	$t4, $zero, '('        	# load '('
 	beq	$t2, $t4, matChkOpPar  	# if (X[k] == '(')  open parenthesis;
-	addi	$t4, $zero, ')'        	# load ')'
+	ori	$t4, $zero, ')'        	# load ')'
 	beq	$t2, $t4, matChkClPar  	# if (X[k] == ')') close parenthesis;
-	addi	$t4, $zero, '*'        	# load '*'
-	beq	$t2, $t4, matChkOptr   	# if (X[k] == '*') operator;
-	addi	$t4, $zero, '+'        	# load '+'
-	beq	$t2, $t4, matChkOptr   	# if (X[k] == '+') operator;
+	ori	$t4, $zero, '*'        	# load '*'
+	beq	$t2, $t4, matChkSlAs   	# if (X[k] == '*') slash or asterisk;
+	ori	$t4, $zero, '+'        	# load '+'
+	beq	$t2, $t4, matChkPlMn   	# if (X[k] == '+') plus or minus;
 matChkCharMns:	# character -
-	addi	$t4, $zero, '-'        	# load '-'
-	beq	$t2, $t4, matChkOptr   	# if (X[k] == '-') operator;
+	ori	$t4, $zero, '-'        	# load '-'
+	beq	$t2, $t4, matChkPlMn   	# if (X[k] == '-') plus or minus;
 matChkCharR20:	# character round 20 [/-9]
-	addi	$t4, $zero, '/'        	# load '/'
-	beq	$t2, $t4, matChkOptr   	# if (X[k] == '/') operator;
+	ori	$t4, $zero, '/'        	# load '/'
+	beq	$t2, $t4, matChkSlAs   	# if (X[k] == '/') slash or asterisk;
 	sltiu	$t3, $t2, '0'          	# if (X[k] < '0')
 	bne	$t3, $zero, matChkInval	#   invalid string;
 	sltiu	$t3, $t2, ':'	       	# if (X[k] <= '9')
 	bne	$t3, $zero, matChkOpnd 	#   operand;
 matChkCharEqu:	# character =
-	addi	$t4, $zero, '='        	# load '='
-	beq	$t2, $t4, matChkOptr   	# if (X[k] == '=') operator;
+	ori	$t4, $zero, '='        	# load '='
+	beq	$t2, $t4, matChkEqls   	# if (X[k] == '=') equals;
 matChkCharR30:	# character round 30 [A-Z]
 	sltiu	$t3, $t2, 'A'          	# if (X[k] < 'A')
 	bne	$t3, $zero, matChkInval	#   invalid string;
@@ -200,8 +212,13 @@ matChkOpPar:	# character is an open parenthesis
 	andi	$t3, $t6, flOpndOptr   	# if (no operator since last operand)
 	bne	$t3, $zero, matChkInval	#   invalid string;
 	addi	$t5, $t5, 1            	# ++i_parentheses
+	ori	$t7, $t7, flDgSt	# start digraph
 	j	matChkChVal            	#  open parenthesis is valid
 matChkClPar:	# character is a close parenthesis
+	andi	$t3, $t6, flClPrOptr   	# if (no operator since last close parenthesis)
+	bne	$t3, $zero, matChkInval	#   invalid string;
+	andi	$t3, $t7, flDgSt	# if (in digraph)
+	bne	$t3, $zero, matChkInval	#   invalid string;
 	addi	$t5, $t5, -1           	# --i_parentheses
 	ori	$t6, $t6, flClPrOptr   	# flag no operator since last closed parenthesis
 	j	matChkChVal            	# close parenthesis is valid
@@ -209,20 +226,30 @@ matChkClPar:	# character is a close parenthesis
 
 ######################################################################
 # # Matlab check: character classes
+matChkEqls:
+	and	$t7, $zero, $zero   	# end diagraph
+	# fall through into operator
 matChkOptr:	# character is an operator
-	add	$t6, $zero, $zero   	# clear both flags
+	add	$t6, $zero, $zero   	# clear both between flags
 	j	matChkChVal	    	# operator is valid
 matChkOpnd:	# character is an operand
 	andi	$t3, $t6, flClPrOptr   	# if (no operator since last close parenthesis)
 	bne	$t3, $zero, matChkInval	#   invalid string;
 	ori	$t6, $t6, flOpndOptr   	# flag no operator since last operand
+	and	$t7, $zero, $zero   	# end diagraph
 	j	matChkChVal	    	# operand is valid
-#	
+matChkSlAs:	# character is slash or asterisk
+	andi	$t3, $t7, flDgSt	# if (in digraph already)
+	bne	$t3, $zero, matChkInval	#   invalid string;
+	# otherwise, fall through into plus, minus
+matChkPlMn:	# character is plus or minus
+	ori	$t7, $t7, flDgSt	# start digraph
+	j	matChkOptr	    	# plus, minus are operators
+#
 
 .data	#  the data block
  matPrompt:	.ascii ">>>\0"	# prompt for input
     inpStr:	.space 64	# the input buffer
-invMessage:	.ascii "Invalid input at: \0\0"	# output for invalid input
+invMessage:	.ascii "Invalid input\0\0\0"	# output for invalid input
 valMessage:	.ascii "Valid input\0"      	# the input buffer
- parErrMsg:	.ascii "Uneven parentheses\0\0"	# c.f., flParError
 # end .data
